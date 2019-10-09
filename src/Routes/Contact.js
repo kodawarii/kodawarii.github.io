14d7a2f9f3278d4b5ef33f799950c01c2f53e3a1
risou <ayamachi@outlook.com>
2019-10-09 23:50:46 +1300

    initial commit - basic layout

---
+++ src/Routes/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component {
+
+  render() {
+    return (
+      <div className="contact">
+        <div> 
+          <p> Email </p>
+          <p> Linkedin </p>
+        </div>
+      </div>
+    );
+  }
+}
